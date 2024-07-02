cca0b4353bb3b6778c64414edb53a6afcd4cd5c4
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2024-07-02 09:06:42 -0400

    Fix precommit to format both ruff and js even if there are no python … (#8)
    * Fix precommit to format both ruff and js even if there are no python files in the repo
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -9,26 +9,45 @@
                 <v-text-field v-model="namespace" label="Namespace" required></v-text-field>
               </v-col>
               <v-col cols="12">
-                <v-file-input v-model="file" label="CSV Mapping" accept=".csv" required show-size outlined>
+                <v-file-input
+                  v-model="file"
+                  label="CSV Mapping"
+                  accept=".csv"
+                  required
+                  show-size
+                  outlined
+                >
                 </v-file-input>
               </v-col>
               <v-col cols="12" class="text-center">
-                <v-btn type="submit" color="#1B335F">
-                  Upload and Create Pull Request
-                </v-btn>
+                <v-btn type="submit" color="#1B335F"> Upload and Create Pull Request </v-btn>
                 <div class="justify-center py-5">
-                  <v-progress-circular v-if="inProgress" indeterminate color="primary"></v-progress-circular>
+                  <v-progress-circular
+                    v-if="inProgress"
+                    indeterminate
+                    color="primary"
+                  ></v-progress-circular>
                 </div>
               </v-col>
             </v-row>
           </v-container>
           <v-fade-transition>
-            <v-alert color="error" icon="$error" title="Error submitting PR" :text="error"
-              v-if="error && !inProgress"></v-alert>
+            <v-alert
+              color="error"
+              icon="$error"
+              title="Error submitting PR"
+              :text="error"
+              v-if="error && !inProgress"
+            ></v-alert>
           </v-fade-transition>
           <v-fade-transition>
-            <v-alert color="success" icon="$success" title="PR Submitted" :text="result"
-              v-if="!error && result !== '' && !inProgress"></v-alert>
+            <v-alert
+              color="success"
+              icon="$success"
+              title="PR Submitted"
+              :text="result"
+              v-if="!error && result !== '' && !inProgress"
+            ></v-alert>
           </v-fade-transition>
         </v-form>
       </v-col>
@@ -37,8 +56,8 @@
 </template>
 
 <script lang="ts">
-import { submitData } from '@/lib/upload';
-import { defineComponent } from 'vue';
+import { submitData } from '@/lib/upload'
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   data() {
@@ -49,34 +68,32 @@ export default defineComponent({
       error: '',
       result: '',
       inProgress: false
-    };
+    }
   },
   methods: {
     async submitForm() {
-      this.error = '';
-      this.result = '';
+      this.error = ''
+      this.result = ''
       if (this.namespace.trim() === '') {
-        this.error = 'Namespace is required';
-        return;
+        this.error = 'Namespace is required'
+        return
       }
 
       if (!this.file) {
-        this.error = 'File is required';
-        return;
+        this.error = 'File is required'
+        return
       }
 
       try {
-        this.inProgress = true;
-        const result = await submitData(this.namespace, this.file);
-        this.result = result;
-      }
-      catch (error) {
-        this.error = error instanceof Error ? error.message : String(error);
-        this.result = '';
+        this.inProgress = true
+        const result = await submitData(this.namespace, this.file)
+        this.result = result
+      } catch (error) {
+        this.error = error instanceof Error ? error.message : String(error)
+        this.result = ''
       }
 
-      this.inProgress = false;
-
+      this.inProgress = false
     }
   }
 })
@@ -84,7 +101,6 @@ export default defineComponent({
 
 <style scoped>
 .form-container {
-
   padding: 20px;
   border-radius: 10px;
   box-shadow: 0 0 80px rgba(0, 0, 0, 0.1);
