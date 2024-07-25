fb62aaea70a00dfe4dce1ecefa3e00494e79f7ae
Benjamin Webb <40066515+webb-ben@users.noreply.github.com>
2024-07-25 19:46:45 -0400

    Add Footer

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -2,12 +2,12 @@
   <div class="header">
     <a href="https://geoconnex.us" class="logo-link">
       <img
-        src="@/assets/logo.png"
+        src="@/assets/logo-geoconnex.png"
         alt="Geoconnex Logo"
-        title="register.geoconnex.us"
-        class="logo-img"
+        title="geoconnex.us"
+        class="logo-geoconnex"
       />
-    </a>
+  </a>
   </div>
 </template>
 
@@ -18,12 +18,14 @@
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   padding: 10px 20px;
   margin-bottom: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.logo-img {
-  width: 180px;
+.logo-geoconnex {
+  width: 480px;
   height: auto;
   /* Maintain aspect ratio */
-  margin-left: 15%;
 }
 </style>
