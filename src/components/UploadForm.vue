ab65b90fdbdbe32a04037dc82ea457e4a2782d96
Benjamin Webb <40066515+webb-ben@users.noreply.github.com>
2024-07-31 11:18:09 -0400

    Update Contributor documentation

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -14,15 +14,26 @@ import URLCheckSummary from '@/components/URLCheckSummary.vue'
                 organization's monitoring locations within a "namespace" (short name for your
                 organization).
               </i>
-              <i class="text-center pa-4"
-                >Ensure your identifiers are well-documented and all info is up-to-date to so that
-                Geoconnex administrators can follow up with you if there are any issues.</i
-              >
-              <v-col cols="10" offset="1">
+              <v-col cols="12">
                 <v-expansion-panels class="contribution-button mb-5">
                   <v-expansion-panel title="CSV Formatting Information" bg-color="#f4f4f9">
                     <v-expansion-panel-text class="expansion-panel text-1b335f">
                       <v-card-text class="markdown-card-text text-center pa-0 ma-0">
+                        <v-row>
+                        <i class="pa-4"
+                          >This tool allows users to create Geoconnex linkages to their own water data.
+                           Submit a CSV file to register persistent URL-formatted identifiers for your 
+                           organization's monitoring locations within a 'namespace' (short name for 
+                           your organization). The features you link to must already exist 
+                           online and have their own web page, if this is not true, learn how to do so
+                           <a href="https://docs.geoconnex.us/reference/data-formats/jsonld/primer/building">here</a>.
+                        </i>
+                        <i class="pa-4"
+                          >Ensure your identifiers are well-documented and all info is up-to-date to so that
+                          Geoconnex administrators can follow up with you if there are any issues.</i
+                        >
+                        </v-row>
+                        <br />
                         The 4 columns in your CSV mapping should be: <br />
                         <code>id target creator description</code>
                         <br />
@@ -31,7 +42,7 @@ import URLCheckSummary from '@/components/URLCheckSummary.vue'
                           target="_blank"
                           variant="test"
                           append-icon="mdi-open-in-new"
-                          href="https://github.com/internetofwater/geoconnex.us/blob/master/namespaces/example-namespace/example_ids.csv"
+                          href="https://github.com/internetofwater/geoconnex.us/blob/master/namespaces/iow/demo.csv"
                           >1:1 example</v-btn
                         >
                         <v-btn
@@ -43,8 +54,7 @@ import URLCheckSummary from '@/components/URLCheckSummary.vue'
                         >
                         <br />
                         For more detailed info, see the
-                        <a href="#contribution">contribution documentation</a> at the bottom of the
-                        page
+                        <a href="https://docs.geoconnex.us/reference/data-formats/csv-submissions/"> CSV formatting documentation</a>
                       </v-card-text>
                     </v-expansion-panel-text>
                   </v-expansion-panel>
